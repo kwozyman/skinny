937ebc7e54db0cc300424814e79d1c7a98b3f2ef
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2024-06-28 15:53:13 +0200

    Define SSH keys for user 'user'

--- bootc.sh
+++ bootc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 mkdir -p output/
+export MYKEY=$(cat ~/.ssh/id_rsa.pub)
+cat config.toml.tmpl | envsubst >config.toml
 podman run \
     --rm \
     -it \
@@ -8,6 +10,7 @@ podman run \
     --security-opt label=type:unconfined_t \
     -v $(pwd)/output:/output \
     -v /var/lib/containers/storage:/var/lib/containers/storage \
+    -v $(pwd)/config.toml:/config.toml \
     registry.redhat.io/rhel9/bootc-image-builder:latest \
     --type raw \
     --local \
