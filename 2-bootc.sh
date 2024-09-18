ded57158b15f6561963e2b703d57cea9353ea6e9
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2024-09-18 14:37:37 +0200

    Adapt for F40 and anaconda-iso with kickstart

--- 2-bootc.sh
+++ 2-bootc.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 mkdir -p output/
 export MYKEY=$(cat ~/.ssh/id_rsa.pub)
-envsubst <config.tmpl.json >config.json
-
-# RHEL one
-IMAGE="registry.redhat.io/rhel9/bootc-image-builder:latest"
 
 # CentOS builder
 IMAGE="quay.io/centos-bootc/bootc-image-builder:latest"
 
 # We use CentOS builder as the one in RHEL is missing the --rootfs parameter that
 # we need to set the system to ext4 until we've a way to specify the disk size
-
-# TODO: Switch back to RHEL builder once --rootfs is added
-# TODO: Once we can determine the disk size created set it and default to whatever FS is used by the tool
 
 podman run \
     --rm \
@@ -23,8 +16,8 @@ podman run \
     --security-opt label=type:unconfined_t \
     -v $(pwd)/output:/output \
     -v /var/lib/containers/storage:/var/lib/containers/storage \
-    -v $(pwd)/config.json:/config.json \
+    -v $(pwd)/config.toml:/config.toml \
     ${IMAGE} \
-    --rootfs ext4 --type raw \
+    --type anaconda-iso \
     --local \
     quay.io/iranzo/skinny:latest
